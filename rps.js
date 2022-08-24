a6cc656f48418ae36b0ff34d040a0da8a1defbab
SoCalStyles <stylesaaronk@gmail.com>
2022-08-24 16:54:12 -0700

    add function to randomly generate option for comp

--- rps.js
+++ rps.js
@@ -1,5 +1,14 @@
 /* Computer will randomly generate rock, paper, or scissors*/
 
 function getComputerChoice() {
-    
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
+    const randomChoice = Math.floor(Math.random()*choices.length);
+    if (randomChoice === 0) {
+        return "ROCK";
+    } else if (randomChoice === 1) {
+        return "PAPER";
+    } else {
+        return "SCISSORS";
+    }
 }
+
