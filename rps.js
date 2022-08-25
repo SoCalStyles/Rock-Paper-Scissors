8d2f45273c4eb5a70b8389147d31305f07698467
SoCalStyles <stylesaaronk@gmail.com>
2022-08-24 17:51:39 -0700

    add function that plays single round

--- rps.js
+++ rps.js
@@ -12,3 +12,38 @@ function getComputerChoice() {
     }
 }
 
+/* Function that plays single round of rock-paper-scissors*/
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === "ROCK") {
+        if (computerSelection === "ROCK") {
+            return "It's a tie!";
+        } else if (computerSelection === "PAPER") {
+            return "Sorry, PAPER beats ROCK. Better luck next time!";
+        } else if (computerSelection === "SCISSORS") {
+            return "You won!";
+        }
+    } else if (playerSelection.toUpperCase() === "PAPER") {
+        if (computerSelection === "ROCK") {
+            return "You won!";
+        } else if (computerSelection === "PAPER") {
+            return "It's a tie!";
+        } else if (computerSelection === "SCISSORS") {
+            return "You Lost! SCISSORS beats PAPER.";
+        }
+    } else if (playerSelection.toUpperCase() === "SCISSORS") {
+        if (computerSelection === "ROCK") {
+            return "Uh oh. ROCK beats SCISSORS!";
+        } else if (computerSelection === "PAPER") {
+            return "You won!";
+        } else if (computerSelection === "SCISSORS") {
+            return "It's a tie!";
+        }
+    } else {
+        return "Invalid Option!";
+    };
+
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
